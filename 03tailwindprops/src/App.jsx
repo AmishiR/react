4b65c69a8c48c086652d35b67961ce3c34128966
Amishi Ranjan <amishiranjan@Amishis-MacBook-Air.local>
2025-08-16 15:38:54 +0530

    Add 03tailwindprops project

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { LavaLamp } from './components/ui/fluid-blob'
+import Example from './components/ui/FloatingPhone'
+
+function App(props) {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="fixed inset-0 w-screen h-screen">
+      <LavaLamp channel="chaiaurcode"/>
+    </div>
+  
+  <div className="relative z-10 w-full h-screen flex items-center justify-center">
+    <Example /> 
+  </div>
+    </>
+  )
+}
+
+export default App
